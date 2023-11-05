ec2cf6047f2825d718cc0d0a5c268d046b5a0f44
Devanhahaha <devanherdiansyah74@gmail.com>
2023-11-05 03:46:26 -0800

    menambahkan singa

---
+++ Singa.php
@@ -0,0 +1,10 @@
+<?php
+class Singa implements Hewan {
+    public function bersuara() {
+        echo "Singa mengeluarkan suara RAAWWRRRR";
+    }
+
+    public function bergerak() {
+        echo "Singa berlari";
+    }
+}
