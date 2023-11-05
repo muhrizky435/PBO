f9988270450ac6ae60f46215849fd9b4a9a6fa31
Zmall02 <nidzhom.akmal22@student.polindra.ac.id>
2023-11-05 18:46:19 +0700

    inisiasi burung

---
+++ Burung.php
@@ -0,0 +1,10 @@
+<?php
+class Burung implements Hewan {
+    public function bersuara() {
+        echo "Burung mengeluarkan suara kukderuuuk";
+    }
+
+    public function bergerak() {
+        echo "Burung terbang";
+    }
+}
